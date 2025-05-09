a856c7e573a7339bb22896442e19ff6df3313bea
abalouri <balouriadbrahman@gmail.com>
2025-05-09 15:23:43 +0100

    gg

--- databases/my.sql
+++ databases/my.sql
@@ -8,3 +8,4 @@ CREATE TABLE users (
     email TEXT UNIQUE,
     password TEXT
 );
+No posts found
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const signUpButton = document.getElementById('signUp');
   const signInButton = document.getElementById('signIn');
   const signupForm = document.getElementById('loginForm')
+  const sign = document.getElementById('sing');
+  const mainPage = document.getElementById('mainPage');
+
 
   signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
@@ -14,4 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
     container.classList.remove('right-panel-active');
     signupForm.style.display='block'
   });
+  sign.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    container.style.display = 'none'; 
+    mainPage.style.display = 'block'; 
+  });
+  
+    
 });
